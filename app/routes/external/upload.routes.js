cca4b01549035ed143ff7464df53185c7f50595c
Muice^^ <moomookingza@gmail.com>
2017-02-15 16:27:43 +0700

    update i18n

--- app/routes/external/upload.routes.js
+++ app/routes/external/upload.routes.js
@@ -1,10 +1,10 @@
 module.exports = function (app) {
 
     var upload = require('../../controllers/external/upload.controller');
+    app.route('/exporter/file/:seller_id').post(upload.uploadFileExporter);
     app.route('/file/:id').delete(upload.deleteFile);
     app.route('/file/:id').get(upload.downloadFile);
     app.route('/list/:refPath/:seller_id').get(upload.listFilePath);
-    app.route('/exporter/file/:seller_id').post(upload.uploadFileExporter);
     app.route('/list/:seller_id').get(upload.listFileDelete);
     app.route('/update/:file_id').put(upload.recoveryFile);
     // app.route('/file').post(upload.uploadFile);
