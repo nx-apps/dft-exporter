7b4dafa6c7fc886f5830827c18e1e138e714780c
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2017-07-13 12:26:28 +0700

    get company

--- app/controllers/external/company.controller.js
+++ app/controllers/external/company.controller.js
@@ -99,24 +99,25 @@ exports.listId = function (req, res) {
         client.GetCompanyProfile(args, function (err, result) {
             var data = result.GetCompanyProfileResult;
             var address = data.CompanyAddress;
+            var bkk = (address.ProvinceEN.toUpperCase() == "BANGKOK" ? true : false);
             var newdata = {
                 company_name_th: data.CompanyNameTH,
                 company_name_en: data.CompanyNameEN,
                 company_taxno: data.CompanyTaxno,
                 company_address_th: address.AddressNo
                 + (address.Moo == "" ? "" : " ม." + address.Moo)
-                + (address.BuildingTH == "" ? "" : " อาคาร" + address.BuildingTH)
+                + (address.BuildingTH == "" ? "" : " " + address.BuildingTH)
                 + (address.SoiTH == "" ? "" : " ซ." + address.SoiTH)
                 + (address.RoadTH == "" ? "" : " ถ." + address.RoadTH)
-                + (address.TumbolTH == "" ? "" : " ต." + address.TumbolTH)
-                + (address.AmphurTH == "" ? "" : " อ." + address.AmphurTH),
+                + (address.TumbolTH == "" ? "" : " " + (bkk ? "แขวง" : "ต.") + address.TumbolTH)
+                + (address.AmphurTH == "" ? "" : " " + (bkk ? "เขต" : "อ.") + address.AmphurTH),
                 company_address_en: address.AddressNo
-                + (address.Moo == "" ? "" : " Village No." + address.Moo)
-                + (address.BuildingEN == "" ? "" : " " + address.BuildingEN + " Building")
-                + (address.SoiEN == "" ? "" : " " + address.SoiEN + " Lane")
+                + (address.Moo == "" ? "" : " Moo." + address.Moo)
+                + (address.BuildingEN == "" ? "" : " " + address.BuildingEN)
+                + (address.SoiEN == "" ? "" : " Soi." + address.SoiEN)
                 + (address.RoadEN == "" ? "" : " " + address.RoadEN + " Road,")
-                + (address.TumbolEN == "" ? "" : " " + address.TumbolEN + " Sub-district,")
-                + (address.AmphurEN == "" ? "" : " " + address.AmphurEN + " District"),
+                + (address.TumbolEN == "" ? "" : " " + address.TumbolEN + ",")
+                + (address.AmphurEN == "" ? "" : " " + address.AmphurEN),
                 company_province_th: address.ProvinceTH
                 + (address.Zipcode == "" ? "" : " " + address.Zipcode),
                 company_province_en: address.ProvinceEN
@@ -124,24 +125,31 @@ exports.listId = function (req, res) {
                 company_fax: data.CompanyFaxNo,
                 company_phone: data.CompanyPhoneNo,
                 company_email: data.CompanyEmail,
-                company_date: new Date(data.JuristicRegDate).toISOString().split('T')[0] + 'T00:00:00+07:00',
-                directors: data.Directors,
-
+                company_date: r.ISO8601(data.JuristicRegDate.toISOString().replace(".000Z", "+07:00")).inTimezone('+07'),
+                directors: data.Directors
             };
-            var newdata = r.expr(newdata).merge({
-                company_date: r.ISO8601(r.row('company_date')).inTimezone('+07')
-            });
-            res.json(newdata);
-            // var db = r.db('external').table('test');
-            // var company = db.getAll(data.CompanyTaxno, { index: 'company_taxno' });
-            // r.branch(company.count().eq(0),
-            //     db.insert(newdata),
-            //     db.get(company(0)('id')).update(newdata)
-            // )
-            //     .run()
-            //     .then(function (datas) {
-            //         res.json(newdata)
-            //     })
+            // res.json(newdata);
+            // var newdata = r.expr(newdata).merge(function (m) {
+            //     return {
+            //         company_date: r.ISO8601(m('company_date')).inTimezone('+07')
+            //     }
+            // });
+
+            // newdata.run().then(function (data) {
+            //     res.json(data);
+            // })
+            var db = r.db('external').table('test');
+            var company = db.getAll(data.CompanyTaxno, { index: 'company_taxno' });
+            r.branch(company.count().eq(0),
+                db.insert(newdata).do(function (d) {
+                    return db.get(d('generated_keys')(0))
+                }),
+                db.get(company(0)('id')).update(newdata).do(function (d) {
+                    return db.get(company(0)('id'))
+                })
+            ).run().then(function (datas) {
+                res.json(datas)
+            })
         });
     });
     // var r = req.r;
