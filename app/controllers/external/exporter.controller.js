f3974c1e2db56c5457fef9b90e2a4629657ce604
Nutthawut-Ice <moomookingza@gmail.com>
2017-07-20 12:01:23 +0700

    update function exporter pluck,without

--- app/controllers/external/exporter.controller.js
+++ app/controllers/external/exporter.controller.js
@@ -40,42 +40,9 @@ exports.exporter = function (req, res) {
                 exporter_status_name: r.branch(m('exporter_status').eq(true), 'เป็นสมาชิก', 'ไม่เป็นสมาชิก'),
                 date_approve: m('date_approve').toISO8601().split('T')(0),
                 date_expire: m('date_expire').toISO8601().split('T')(0),
-                // company: m('company').merge(function (company_merge) {
-                //     return {
-                //         company_agent: r.branch(company_merge.hasFields('company_agent'),
-                //             company_merge('company_agent').pluck('TitleNameTH', 'FirstNameTH', 'LastNameTH'),
-                //             []
-                //         )
-                //     }
-                // }),
                 export_status: r.branch(m('date_expire').gt(r.now()), true, false)
-                // exporter_date_expire: r.time(m('exporter_date_approve').year().add(1),
-                //     m('exporter_date_approve').month(),
-                //     m('exporter_date_approve').day(),
-                //     "+07:00"
-                // ).toISO8601(),
-                // date_export_expire: r.branch(m.hasFields('date_exported'), r.time(m('date_exported').year().add(1),
-                //     m('date_exported').month(),
-                //     m('date_exported').day(),
-                //     "+07:00"
-                // ).toISO8601(),
-                //     null),
-                // date_exported: r.branch(m.hasFields('date_exported'), m('date_exported').toISO8601(), null),
-
             }
         })
-        // .merge(function (mm) {
-        //     return {
-        //         export_date_expire: r.branch(mm('date_export_expire').gt(mm('exporter_date_expire')),
-        //             r.branch(mm('date_export_expire').ne(null), mm('date_export_expire').split('T')(0), null),
-        //             mm('exporter_date_expire').split('T')(0))
-        //     }
-        // }).without('date_export_expire', 'exporter_date_expire')
-        // .merge(function (mmm) {
-        //     return {
-        //         export_status: r.branch(mmm('date_expire').gt(r.now().toISO8601().split('T')(0)), true, false)
-        //     }
-        // })
         .merge(function (m) {
             return {
                 export_status_name: r.branch(m('export_status').eq(true), 'ปกติ', 'หมดอายุ')
@@ -85,10 +52,10 @@ exports.exporter = function (req, res) {
         .filter(d)
         .orderBy('exporter_no');
     if (typeof req.query.pluck !== 'undefined') {
-        table = table.pluck(req.query.pluck);
+        table = table.pluck(r.args(split(req.query.pluck)));
     }
     if (typeof req.query.without !== 'undefined') {
-        table = table.without(req.query.without);
+        table = table.without(r.args(split(req.query.without)));
     }
     if (!isNaN(skip)) {
         table = table.skip(skip).limit(limit);
@@ -326,4 +293,8 @@ exports.page = function (req, res) {
         .catch(function (err) {
             res.json(err);
         })
+}
+
+function split(data){
+    return data.split(',')
 }
