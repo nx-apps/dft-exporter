7fabe2797e48c8ca468299c88940995d9c10b407
tawatchai.w <tawatchai.w@nextcorp.co.th>
2017-08-31 11:44:02 +0700

    upload file

--- app/controllers/file.controller.js
+++ app/controllers/file.controller.js
@@ -10,35 +10,37 @@ exports.upload = function (req, res) {
         var doc_type_id = req.headers['doc-type-id'];
         var company_taxno = req.headers['company-taxno'];
         var draft_status = req.headers['draft-status'];
+        var draft_id = req.headers['draft-id'];
+        // if (draft_id == '') {
+        fs.readFile(prefile.path, function (err, data) {
+            // console.log(r);
+            r.db('external').table('file').insert({
+                filename: prefile.originalFilename.split('.')[0] + '_' + new Date().getTime() + "." + prefile.originalFilename.split('.')[1],
+                filetype: prefile.headers['content-type'],
+                contents: data,
+                date_upload: r.now().inTimezone('+07'),
+                doc_type_id: doc_type_id
+            })('generated_keys')(0)
+                .do(function (file_id) {
+                    return r.db('external').table('doc_temp').insert(
+                        r.table('file').get(file_id).without('contents', 'id').merge({
+                            file_id: file_id,
+                            file_status: true,
+                            company_taxno: company_taxno,
+                            draft_status: draft_status
+                            // date_created: r.now().inTimezone('+07'),
+                            // date_updated: r.now().inTimezone('+07')
+                        })
+                    )
+                })
+                .run().then(function (result) {
+                    res.json(result);
+                }).catch(function (err) {
+                    res.json(err);
+                })
+        });
+        // }
 
-        res.json(prefile)
-        /*  fs.readFile(prefile.path, function (err, data) {
-              // console.log(r);
-              r.db('external').table('file').insert({
-                  filename: prefile.originalFilename.split('.')[0] + '_' + new Date().getTime() + "." + prefile.originalFilename.split('.')[1],
-                  filetype: prefile.headers['content-type'],
-                  contents: data,
-                  date_upload: r.now().inTimezone('+07'),
-                  doc_type_id: doc_type_id
-              })('generated_keys')(0)
-                  .do(function (file_id) {
-                      return r.db('external').table('doc_temp').insert(
-                          r.table('file').get(file_id).without('contents', 'id').merge({
-                              file_id: file_id,
-                              file_status: true,
-                              company_taxno: company_taxno,
-                              draft_status: draft_status,
-                              date_created: r.now().inTimezone('+07'),
-                              date_updated: r.now().inTimezone('+07')
-                          })
-                      )
-                  })
-                  .run().then(function (result) {
-                      res.json(result);
-                  }).catch(function (err) {
-                      res.json(err);
-                  })
-          }); */
     });
 
 }
