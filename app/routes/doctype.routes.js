735380bbd7bc07229cb0ff9368a42dad6df4ff7e
tawatchai.w <tawatchai.w@nextcorp.co.th>
2017-09-01 14:17:41 +0700

    crud doctype

--- app/routes/doctype.routes.js
+++ app/routes/doctype.routes.js
@@ -1,5 +1,8 @@
 module.exports = function (app) {
     var controller = require('../controllers/doctype.controller');
     app.get(['/', '/list'], controller.list);
-   
+    app.get('/get', controller.get);
+    app.post('/insert', controller.insert);
+    app.put('/update', controller.update);
+    app.delete('/delete', controller.delete);
 }
