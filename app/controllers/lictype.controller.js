13446504dbc32f8fe5c5c382779e000d13cdeff7
tawatchai.w <tawatchai.w@nextcorp.co.th>
2017-09-01 14:10:53 +0700

    crud lictype

--- app/controllers/lictype.controller.js
+++ app/controllers/lictype.controller.js
@@ -5,3 +5,37 @@ exports.list = function (req, res) {
             res.json(data)
         })
 }
+exports.get = function (req, res) {
+    r.table('license_type')
+        .get(req.query.id)
+        .run()
+        .then(function (data) {
+            res.json(data)
+        })
+}
+exports.insert = function (req, res) {
+    r.table('license_type')
+        .insert(req.body)
+        .run()
+        .then(function (data) {
+            res.json(data)
+        })
+}
+exports.update = function (req, res) {
+    r.table('license_type')
+        .get(req.body.id)
+        .update(req.body)
+        .run()
+        .then(function (data) {
+            res.json(data)
+        })
+}
+exports.delete = function (req, res) {
+    r.table('license_type')
+        .get(req.query.id)
+        .delete()
+        .run()
+        .then(function (data) {
+            res.json(data)
+        })
+}
--- app/routes/lictype.routes.js
+++ app/routes/lictype.routes.js
@@ -1,4 +1,8 @@
 module.exports = function (app) {
     var controller = require('../controllers/lictype.controller');
     app.get(['/', '/list'], controller.list);
+    app.get('/get',controller.get);
+    app.post('/insert', controller.insert);
+    app.put('/update', controller.update);
+    app.delete('/delete', controller.delete);
 }
