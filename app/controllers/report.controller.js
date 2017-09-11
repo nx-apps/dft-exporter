3bae90613c0f814c602fafef0d425518f057a823
tawatchai.w <tawatchai.w@nextcorp.co.th>
2017-09-11 20:34:46 +0700

    report1 short function

--- app/controllers/report.controller.js
+++ app/controllers/report.controller.js
@@ -6,15 +6,40 @@ var tz = "T00:00:00.000+07:00";
 var d1y = (y - 1) + '-' + (m < 9 ? '0' : '') + (m + 1) + '-' + (d < 10 ? '0' : '') + d + tz;
 exports.report1 = function (req, res) {
     var r = req.r;
-    // res.json(__dirname.replace('controller','report'));
     var parameters = {
         CURRENT_DATE: new Date().toISOString().slice(0, 10),
         date_start: req.query.date_start,
         date_end: req.query.date_end
     };
-    var q = {}, d = {}, o = r.desc('exporter_no');
+    var _query = _boolean(req.query);
+    var o = r.desc('exporter_no');
     var tb = r.db('external').table('exporter');
-    for (key in req.query) {
+    var data = _date(_query, tb, o);
+    data.tb.filter(_query.q)
+        .map(function (m) {
+            return m.pluck('exporter_no', 'id', 'date_load', 'date_expire')
+                .merge({
+                    company_name_th: m('company')('company_name_th'),
+                    lic_type_name: m('lic_type')('lic_type_name'),
+                    company_taxno: m('company')('company_taxno'),
+                    date_load2: m('date_load').inTimezone('+07').toISO8601().split('T')(0),
+                    date_expire2: m('date_expire').inTimezone('+07').toISO8601().split('T')(0),
+                    exporter_no_name: m('lic_type')('lic_type_prefix').add(m('exporter_no').coerceTo('string'))
+                })
+        })
+        .orderBy(data.o)
+        .run()
+        .then(function (result) {
+            res.ireport("exporter/report1.jasper", req.query.export || "pdf", result, parameters);
+        })
+        .error(function (err) {
+            res.json(err)
+        })
+}
+
+function _bool(p) {
+    var data = {}, q = {}, d = {};
+    for (key in p) {
 
         if (req.query[key] == "true") {
             req.query[key] = true;
@@ -30,40 +55,22 @@ exports.report1 = function (req, res) {
             q[key] = req.query[key];
         }
     }
-    // console.log(parameters, d);
-    if (Object.getOwnPropertyNames(d).length !== 0) {
-        // parameters['date_start'] = d['date_start'];
-        // parameters['date_end'] = d['date_end'];
-        if (req.query.export_status !== 'undefined' && req.query.export_status == false) {
-            tb = tb.between(r.ISO8601(d.date_start), r.ISO8601(d.date_end).add(1), { index: 'date_expire' })
-            // d = r.row('date_expire').ge(r.ISO8601(d.date_start)).and(r.row('date_expire').le(r.ISO8601(d.date_end)));
+    data['q'] = q;
+    data['d'] = d;
+    return data;
+}
+function _date(p, tb, o) {
+    var data = {};
+    if (Object.getOwnPropertyNames(p.d).length !== 0) {
+        if (p.q.export_status !== 'undefined' && p.q.export_status == false) {
+            tb = tb.between(r.ISO8601(p.d.date_start), r.ISO8601(p.d.date_end).add(1), { index: 'date_expire' });
             o = r.asc('date_expire');
         } else {
-            tb = tb.between(r.ISO8601(d.date_start), r.ISO8601(d.date_end).add(1), { index: 'date_load' })
-            // d = r.row('date_load').ge(r.ISO8601(d.date_start)).and(r.row('date_load').le(r.ISO8601(d.date_end)));
+            tb = tb.between(r.ISO8601(p.d.date_start), r.ISO8601(p.d.date_end).add(1), { index: 'date_load' });
             o = r.asc('date_load');
         }
     }
-    tb.filter(q)
-        .map(function (m) {
-            return m.pluck('exporter_no', 'id', 'date_load', 'date_expire')
-                .merge({
-                    company_name_th: m('company')('company_name_th'),
-                    lic_type_name: m('lic_type')('lic_type_name'),
-                    company_taxno: m('company')('company_taxno'),
-                    // exporter_status: r.branch(m('exporter_status').eq(true), 'เป็นสมาชิก', 'ไม่เป็นสมาชิก'),
-                    date_load2: m('date_load').inTimezone('+07').toISO8601().split('T')(0),
-                    date_expire2: m('date_expire').inTimezone('+07').toISO8601().split('T')(0),
-                    exporter_no_name: m('lic_type')('lic_type_prefix').add(m('exporter_no').coerceTo('string'))
-                })
-        })
-        .orderBy(o)
-        .run()
-        .then(function (result) {
-            // res.json(result);
-            res.ireport("exporter/report1.jasper", req.query.export || "pdf", result, parameters);
-        })
-        .error(function (err) {
-            res.json(err)
-        })
+    data['tb'] = tb;
+    data['o'] = o;
+    return data;
 }
--- app/routes/report.routes.js
+++ app/routes/report.routes.js
@@ -1,5 +1,5 @@
 module.exports = function (app) {
     var controller = require('../controllers/report.controller');
     app.get('/report1', controller.report1)
-
+    // app.get('/report2', controller.report2)
 }
